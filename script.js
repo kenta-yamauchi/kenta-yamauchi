72f6c0d49e70519d7f7bff020a9f394038658822
kenta-yamauchi <63192161+kenta-yamauchi@users.noreply.github.com>
2020-04-05 21:14:09 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$('#top-btn').click(function() {
+    $('html,body').animate({
+        'scrollTop':0
+    },500);
+});
+
+$('.scroll-btn').click(function() {
+var id = $(this).attr('href');
+
+var position = $(id).offset().top;
+$('html,body').animate({
+    'scrollTop' : position
+}, 1000)
+});
+
